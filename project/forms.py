82199732a45115c08d4d7544af8048709db26e6b
JesperLyhneLind <100363999+JesperLyhneLind@users.noreply.github.com>
2023-08-04 00:33:39 +0200

    Delete project directory

--- project/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, SelectField
-from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
-
-class CustomerLoginForm(FlaskForm):
-    id = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
